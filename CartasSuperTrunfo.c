706139d21bf20d0262194a2199f6e94cf8a0aa82
mialcantara25-sketch <mi.alcantara25@gmail.com>
2026-04-04 02:38:30 +0000

    desafio de cartas nivel novato concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,59 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+typedef struct {
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+} Pais;
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    Pais p1, p2;
 
-  // Área para entrada de dados
+    // Cadastro da Carta 1
+    printf("--- Cadastro da Carta 1 ---\n");
 
-  // Área para exibição dos dados da cidade
+    printf("Nome do País: \n ");//Lê a string com espaços
+    scanf(" %[^\n]s", p1.nome); 
+
+    printf("População: \n ");//Lê a string com espaços
+    scanf(" %[^\n]f", &p1.populacao);
+
+    printf("Área (km²): \n ");//Lê a string com espaços
+    scanf(" %[^\n]e", &p1.area);
+
+    printf("PIB (bilhões): \n ");//Lê a string com espaços
+    scanf(" %[^\n]f", &p1.pib);
+
+    printf("Número de pontos turísticos: \n ");//Lê a string com espaços
+    scanf(" %[^\n]s", &p1.pontos_turisticos);
+
+    printf("\n");
+
+    // Cadastro da Carta 2
+    printf("--- Cadastro da Carta 2 ---\n");
+
+    printf("Nome do País: \n ");//Lê a string com espaços
+    scanf(" %[^\n]s", p2.nome); 
+    
+    printf("População: \n");//Lê a string com espaços
+    scanf(" %[^\n]f", &p2.populacao);
+
+    printf("Área (km²): \n ");//Lê a string com espaços
+    scanf("%[^\n]e", &p2.area);
+
+    printf("PIB (bilhões): \n ");//Lê a string com espaços
+    scanf(" %[^\n]e", &p2.pib);
+
+    printf("Número de pontos turísticos: \n");//Lê a string com espaços
+    scanf(" %[^\n]s", &p2.pontos_turisticos);
+
+   
+    
+
+
 
 return 0;
 } 
